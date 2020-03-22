01f45780a08ef32e76e38ed02aa8bb4cf90f59ed
Fu-Sheng <rich840213@gmail.com>
2020-03-22 23:33:25 +0800

    function design, toast, dialog

--- app/src/main/java/com/jk/guess/MainActivity.kt
+++ app/src/main/java/com/jk/guess/MainActivity.kt
@@ -1,13 +1,39 @@
 package com.jk.guess
 
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = MainActivity::class.java.simpleName
+    private val secretNumber = SecretNumber()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "secret: ${secretNumber.secret}")
 
+        button_ok.setOnClickListener {
+            val n = ed_number.text.toString().toInt()
+            val diff = secretNumber.validate(n)
+            var message = "Yes, you got it"
+            Log.d(TAG, n.toString())
+
+            if (diff < 0) {
+                message = "Bigger"
+            } else if (diff > 0) {
+                message = "Smaller"
+            }
+
+//            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+            AlertDialog.Builder(this)
+                .setTitle("Message")
+                .setMessage(message)
+                .setPositiveButton("OK", null)
+                .show()
+        }
     }
 }
