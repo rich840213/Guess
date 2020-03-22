8ce8c1ca17b5744d347f7e2d9c476518adb4e0dc
Fu-Sheng <rich840213@gmail.com>
2020-03-22 22:39:29 +0800

    initial commit

---
+++ app/src/main/java/com/jk/guess/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.jk.guess
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
